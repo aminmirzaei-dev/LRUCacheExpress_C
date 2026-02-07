2de67c66fbf8fd444f7c5b2511447af2f0f814e9
Amin Mirzaei <aminmirzaeidev@gmail.com>
2026-02-08 03:07:43 +0330

    Add header file for LRU cache implementation

--- sources/lru_cache.h
+++ sources/lru_cache.h
@@ -1,1 +1,14 @@
+#ifndef LRU_CACHE_H
+#define LRU_CACHE_H
+
+#include <stddef.h>
+
+typedef struct lru_cache lru_cache_t;
+
+lru_cache_t* lru_create(size_t capacity);
+int lru_put(lru_cache_t *cache, int key, int value);
+int lru_get(lru_cache_t *cache, int key, int *out_value);
+void lru_destroy(lru_cache_t *cache);
+
+#endif
 
