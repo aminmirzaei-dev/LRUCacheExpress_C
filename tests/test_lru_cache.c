3fcf67707a05ea1e06b872297e0ec17bf81ff52b
Amin Mirzaei <aminmirzaeidev@gmail.com>
2026-02-08 03:10:23 +0330

    Add test for LRU cache functionality

--- tests/test_lru_cache.c
+++ tests/test_lru_cache.c
@@ -1,1 +1,24 @@
+#include "../include/lrucacheexpress.h"
+#include <stdio.h>
+
+int main(void)
+{
+    lru_cache_t *cache = lru_create(2);
+    int value;
+
+    lru_put(cache, 1, 10);
+    lru_put(cache, 2, 20);
+
+    lru_get(cache, 1, &value);
+    printf("%d\n", value);
+
+    lru_put(cache, 3, 30);
+
+    if (lru_get(cache, 2, &value) != 0) {
+        printf("Key 2 evicted\n");
+    }
+
+    lru_destroy(cache);
+    return 0;
+}
 
